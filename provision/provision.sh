b31fd137a782e673573e23caa7b23fd619be7739
john mccallum <john.mccallum@plantandfood.co.nz>
2014-10-13 08:02:19 +1300

    added vcflib

--- provision/provision.sh
+++ provision/provision.sh
@@ -8,11 +8,6 @@ if ! type "primer3" 2> /dev/null; then
   sudo apt-get -y install primer3
 fi
 
-if ! type "tabix" 2> /dev/null; then
-  sudo apt-get -y install tabix
-fi
-
-
 ## set up for Gisting
 sudo gem install gist
 
@@ -20,6 +15,8 @@ sudo gem install gist
 # sudo apt-get -y install screen
 # curl -o ~/.screenrc https://gist.githubusercontent.com/cfljam/610e9fe0a8ca23e80199/raw
 
+### install  R2Py,PyBedtools and PyVCF - should go to puppet
+sudo pip install rpy2 pyvcf
 
 ##install bedtools
 if ! type "bedtools" 2> /dev/null; then
@@ -34,6 +31,8 @@ git pull
 sudo python setup.py develop
 cd
 
+
+
 ## install vcftools
 if ! type "vcftools" 2> /dev/null; then
     echo "Installing vcftools"
@@ -43,10 +42,27 @@ if ! type "vcftools" 2> /dev/null; then
     make
     sudo cp ./bin/* /usr/local/bin
     sudo cp ./perl/* /usr/local/bin
-    echo "export PERL5LIB=$PERL5LIB:/usr/local/bin" >> ~/.bashrc  
+    echo "export PERL5LIB=$PERL5LIB:/usr/local/bin" >> ~/.bashrc
     cd ..
-    rm -rf vcftools_0.1.12b
+    rm -rf vcftools_0.1.12b*
 fi
 
-### install  R2Py,PyBedtools and PyVCF - should go to puppet
-sudo pip install rpy2 pyvcf
+### Install vcflib
+if ! type "vcfcheck" 2> /dev/null; then
+  git clone --recursive git://github.com/ekg/vcflib.git
+  cd vcflib
+  make
+  sudo cp bin/* /usr/local/bin
+  cd
+  rm -rf vcflib
+fi
+
+### Install BCFTools
+if ! type "bcftools" 2> /dev/null; then
+    git clone  git://github.com/samtools/htslib.git htslib
+    git clone git://github.com/samtools/bcftools.git bcftools
+    cd htslib; git pull; cd ..
+    cd bcftools; git pull; cd ..
+    cd bcftools; make; make test; sudo make install
+    cd
+fi
