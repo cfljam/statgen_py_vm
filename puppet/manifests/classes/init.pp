90572936deb837acc4a6e5a398b156b3e4737f99
cfljam <john.mccallum@plantandfood.co.nz>
2014-10-17 11:40:48 +1300

    added rcurl deps as requested by Murray

--- puppet/manifests/classes/init.pp
+++ puppet/manifests/classes/init.pp
@@ -22,7 +22,7 @@ class init {
                     Exec['update_apt'],
                 ];
             }
-            $misc_packages = ["make", "curl", "git-core","autoconf","ruby"]
+            $misc_packages = ["make", "curl", "git-core","autoconf","ruby","libxml2-dev", "libcurl4-openssl-dev"]
             package { $misc_packages:
                 ensure => present,
                 require => [
